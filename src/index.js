0a53d45ec2198c09eaacb05212449cb6ccfdff82
mir97 <78717265+mir97@users.noreply.github.com>
2021-02-24 08:10:43 +0300

    init: start Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketConfig = bracketsConfig.map((config) => config.join(""));
+  function bracketRecursion() {
+      if (str.length == 0) return true;
+      for (const conf of bracketConfig)
+          if (str.includes(conf))
+              return bracketRecursion(
+                  (str = str.replace(conf, "")),
+                  bracketConfig
+              );
+
+      return false;
+  }
+  return bracketRecursion(str, bracketConfig);
+};
